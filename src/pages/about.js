0fef4ffacc05609b9ee5658c10ae37001ec03241
jesussandrea <jesussandrea12@gmail.com>
2017-11-28 08:18:21 -0400

    initial commit

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Menu from '../components/menu';
+
+const About = () => (
+  <section>
+    <Menu />
+    <h3>About BloggerTech</h3>
+    <p>Aprende de expertos en tecnologia, diseño, marketing, startups y web</p>
+  </section>
+);
+
+export default About;
---
+++ src/pages/home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Menu from '../components/menu';
+
+const Home = () => (
+  <section>
+    <Menu />
+    <h3>Home BloggerTech</h3>
+    <p>Aprende de expertos en tecnologia, diseño, marketing, startups y web</p>
+  </section>
+);
+
+export default Home;
